00d9806a08545c70adf17090afd90343ac0ef716
Chumou Zhang <chumouzhang@MacBook-Pro.fios-router.home>
2021-11-02 12:35:04 -0400

    update

--- index.js
+++ index.js
@@ -9,66 +9,67 @@ let http = require('http');
 let server = http.createServer(app);
 let port = process.env.PORT || 3000;
 server.listen(port, () => {
-    console.log("Server is listening at: " + port);
+  console.log("Server is listening at: " + port);
 });
 
 //STEP 3. Socket connection
 let io = require('socket.io');
 io = new io.Server(server);
 
-//Establish socket connection
+//Establish default socket connection
 io.sockets.on('connection', (socket) => {
-    console.log("We have a new client: " + socket.id);
+  console.log("We have a new client: " + socket.id);
 
-    //STEP 6. Listen for data
-    socket.on("data", (data) => {
-        console.log(data);
+  //STEP 6. Listen for data
+  socket.on("data", (data) =>{
+    console.log(data);
 
-        //send to all clients, including myself
-        io.sockets.emit('draw-data', data);
-    });
+    //send to all clients, including myself
+    io.sockets.emit('draw-data', data);
+  });
 
-    socket.on('disconnect', () => {
-        console.log('Client disconnected: ' + socket.id);
-    });
+  socket.on('disconnect', () =>{
+    console.log('Client disconnected: ' + socket.id);
+  });
 });
 
-
-//private namespace connection 
+//private namespace connection
 let private = io.of('/private');
 private.on('connection', (socket) => {
-    console.log("We have a new client: " + socket.id);
+  console.log("We have a new client: " + socket.id);
 
-    //listen for room name
-    socket.on('room-name', data => {
-        console.log(data);
+  //listen for room name
+  socket.on('room-name', data => {
+    console.log(data);
 
-        //add socket to room
-        socket.join(data.room);
+    //add socket to room
+    socket.join(data.room);
 
-        //add room property to socket object
-        socket.room = data.room;
-        console.log(socket);
+    //add room property to socket object
+    socket.room = data.room;
+    // console.log(socket);
 
-        //send a welcome message to new client
-        let welcomMeg = "Welcome to'" + data.room + "'room!";
+    //send a welcome message to new clients
+    let welcomeMsg = "Welcome to '" + data.room + "' room!";
 
-        // private.to(socket.room).emit('joined',{msg:welcomMeg});
-        socket.emit('joined',{msg:welcomMeg});
-    });
+    //send a welcome message to everybody in a room
+    // private.to(socket.room).emit('joined', {msg: welcomeMsg});
+    socket.emit('joined', {msg: welcomeMsg});
+  });
 
-    //STEP 6. Listen for data
-    socket.on("data", (data) => {
-        console.log(data);
+  //STEP 6. Listen for data
+  socket.on("data", (data) =>{
+    console.log(data);
 
-        //send to all clients, including myself
-        private.to(socket.room).emit('darw-data', data);
-    });
+    //send to all clients, including myself
+    private.to(socket.room).emit('draw-data', data);
+  });
 
-    socket.on('disconnect', () => {
-        // console.log('Client disconnected' + socket.id);
-        console.log('Client' + socket.id + 'dissconnected and left room' + socket.room);
+  socket.on('disconnect', () =>{
+    // console.log('Client disconnected: ' + socket.id);
+    // console.log('Client ' + socket.id + ' disconnected and left room ' + socket.room);
+    console.log(`Client ${socket.id} disconencted and left room ${socket.room}`);
 
-        socket.leave(socket.room);
-    })
-})
+    socket.leave(socket.room);
+  });
+});
--- public/private/app_private.js
+++ public/private/app_private.js
@@ -4,49 +4,54 @@ let r;
 let g;
 let b;
 
-window.addEventListener('load',()=>{
-    let roomName = window.prompt('Enter room name:')
-    console.log()
-})
+window.addEventListener('load', () => {
+  let roomName = window.prompt('Enter room name: ')
+  console.log(roomName);
 
-socket.on('connect',()=>{
-    console.log('Connected');
-})
+  //send room name to server
+  socket.emit('room-name', {room: roomName});
+});
 
-//STEP 8. listen for data form the server
-socket.on('darw-data',(data)=>{
-    console.log(data);
-    drawObj(data);
-})
+socket.on('connect', () =>{
+  console.log('Connected');
+});
 
-// STEP 1.
+socket.on('joined', (data) => {
+  console.log(data.msg);
+});
+
+//STEP 8. Listen for data from the server
+socket.on('draw-data', (data) => {
+  console.log(data);
+  drawObj(data);
+});
+
+//STEP 1. Set up basic app functionality
 function setup(){
-    createCanvas(windowWidth,windowHeight);
-    background(255)
+  let myCanvas = createCanvas(windowWidth, windowHeight);
+  myCanvas.parent("canvas-container");
+  background(255);
 
-    r = random(255);
-    g = random(255);
-    b = random(255);
+  r = random(255);
+  g = random(255);
+  b = random(255);
 }
 
 function mouseMoved(){
-    // fill(0);
-    // ellipse(mouseX,mouseY,30);
-
-    //data object
-    let mousePos = {
-        x: mouseX,
-        y: mouseY,
-        "red" : r,
-        "green" : g,
-        "blue" : b
-    }
-    // console.log(mousePos);
-    //STEP 5. emit the data
-    socket.emit('data',mousePos);
+  //data object
+  let mousePos = {
+    x: mouseX,
+    y: mouseY,
+    "red": r,
+    "green": g,
+    "blue": b
+  }
+  //console.log(mousePos);
+  //STEP 5. emit the data
+  socket.emit('data', mousePos);
 }
 
 function drawObj(pos){
-    fill(pos.red,pos.green,pos.blue);
-    ellipse(pos.x,pos.y,30);
+  fill(pos.red, pos.green, pos.blue);
+  ellipse(pos.x, pos.y, 30);
 }
